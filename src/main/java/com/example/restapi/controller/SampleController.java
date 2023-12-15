ec477928c9910a0fbe2cfee88634026fd5d4a83d
Amit Kumar <amitrke@gmail.com>
2023-12-15 10:52:59 -0800

    Add log4j2 configuration and logging in SampleController

--- src/main/java/com/example/restapi/controller/SampleController.java
+++ src/main/java/com/example/restapi/controller/SampleController.java
@@ -3,12 +3,17 @@ package com.example.restapi.controller;
 import com.example.restapi.model.APIResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 @RestController
 public class SampleController {
 
+    private static final Logger logger = LogManager.getLogger(SampleController.class);
+
     @GetMapping("/status")
     public APIResponse status() {
+        logger.info("This is a message");
         return new APIResponse();
     }
 }
